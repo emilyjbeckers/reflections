78106b62f068477480d45b24982bf59ca978fc93
Emily J Beckers <emilyjbeckers@gmail.com>
2018-11-12 16:07:22 -0500

    added start and end tags. begin differentiation of voices

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is an algorithmic composition piece in which the code sonifies itself. Styl
 The code is written using a framework called [Sonic Pi](https://sonic-pi.net) which is a Ruby wrapper around the more well-known [SuperCollider](https://supercollider.github.io). While in development, the only way to play the piece will be within a Sonic Pi environment. I've been using [this third-party CLI](https://github.com/lpil/sonic-pi-tool) rather than the official interface. **This program will not run in a standard Ruby environment.**
 
 There will be a bounce provided, but I do not guarantee that it is always from the most current version. 
+
+File will play between starting tag `#>>START-HERE` and ending tag `#>>END-HERE`, inclusive. These tags can be moved for testing purposes but should never be committed in any positions other than the first and last lines of the file respectively.
--- reflections.rb
+++ reflections.rb
@@ -1,5 +1,19 @@
+#>>START-HERE
+# The above line MUST be the first line in the file (it can be moved for testing purposes).
+
 # Reflections is a piece that is meant to use algorithmic composition to sonify itself.
-# It uses a framework called Sonic Pi that from what I understand is a Ruby wrapper around SuperCollider.
+# It uses a framework called Sonic Pi that from what I understand is a Ruby wrapper around SuperCollider. This code will not run in a standard ruby enviroment.
+
+RESERVED_WORDS = ['__ENCODING__', '__LINE__', '__FILE__', 'BEGIN', 'END', 'alias', 'and', 'begin', 'break', 'case', 'class', 'def', 'defined?', 'do', 'else', 'elsif', 'end', 'ensure', 'false', 'for', 'if', 'in', 'module', 'next', 'nil', 'not', 'or', 'redo', 'rescue', 'retry', 'return', 'self', 'super', 'then', 'true', 'undef', 'unless', 'until', 'when', 'while', 'yield']
+
+VOICES = {
+  normal: {synth: :saw},
+  comment: {synth: :fm, play_opts: {divisor: 1, depth: 0.5, attack_level: 0.8}},
+  keyword: {},
+}
+MODIFIERS = [:string, :method, :block, :parens]
+
+Instruction = Struct.new(:pitch, :voice, :modifiers)
 
 # Responsible for rendering out the music it is given
 class Renderer
@@ -16,14 +30,18 @@ class Renderer
     }
   end
 
-  def play_note(pitch, opts = {})
-    @sp.play pitch, opts
+  def play_note(instruction)
+    if VOICES[instruction.voice][:synth]
+      @sp.use_synth VOICES[instruction.voice][:synth]
+    end
+
+    @sp.play instruction.pitch, VOICES[instruction.voice][:play_opts] || {}
   end
 end
 
 # Responsible for parsing the code as given
 class Parser
-  # Load this file
+  # Take in the sonic pi context to have access to its methods
   def initialize(sp)
     @sp = sp
     @instructions = []
@@ -33,12 +51,35 @@ class Parser
     # Note: This will break if the file has been renamed.
     f = File.open(File.absolute_path(__dir__) + '/reflections.rb')
 
-    f.read.each_char{ |letter|
-      letters = 'abcdefghijklmnopqrstuvwxyz'
-      @instructions << letters.index(letter) + 59
+    started = false
+    f.read.each_line{ |line|
+      if started || line.match(/#>>START-HERE\s*$/)
+        started = true
+        parse_line line
+      end
+
+      break if line.match(/#>>END-HERE\s*$/)
     }
 
     f.close
+  end
+
+  def parse_line(line)
+
+    voice = line.match(/^\s*#/) ? :comment : :normal
+
+    line.each_line(' '){ |word|
+      voice = RESERVED_WORDS.include?(word) && voice != :normal ? :keyword : voice
+
+      word.each_char{ |letter|
+        @instructions << Instruction.new(get_pitch(letter), voice)
+      }
+    }
+  end
+
+  def get_pitch(letter)
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    return letters.index(letter.downcase) + 59
   end
 
   def instructions
@@ -51,8 +92,13 @@ end
 parser = Parser.new self
 parser.parse_file
 
+print "Number of instructions: #{parser.instructions}"
+
 renderer = Renderer.new self
 
 renderer.play_instructions parser.instructions
 
 # eeeeeeeeeeeeeeeeeee
+
+# The below line MUST be the last line in the file (it can be moved for testing purposes).
+#>>END-HERE
