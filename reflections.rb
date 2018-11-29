64328533712fcdbf074bdbc016f80740c6672791
Emily J Beckers <emilyjbeckers@gmail.com>
2018-11-29 18:47:50 -0500

    characters in symbols played simultaneously. fixed modifiers. updated uppercase rules. updated whitespace.

--- reflections.rb
+++ reflections.rb
@@ -7,14 +7,14 @@
 RESERVED_WORDS = ['break', 'case', 'class', 'def', 'do', 'else', 'elsif', 'end','false', 'for', 'if', 'nil', 'redo', 'return', 'self', 'then', 'true', 'when', 'while', 'yield']
 
 VOICES = {
-  normal: {synth: :fm, play_opts: {amp: 0.6, attack: 0.01}},
-  comment: {synth: :fm, play_opts: {divisor: 1, depth: 0.5, attack_level: 0.7, attack: 0.15, amp: 0.5}},
-  special_character: {synth: :saw},
+  normal: { synth: :fm , play_opts: { amp: 0.6, attack: 0.01, release: 0.6}},
+  comment: { synth: :fm , play_opts: { divisor: 1, depth: 0.5, attack_level: 0.7, attack: 0.15, amp: 0.5}},
+  special_character: { synth: :saw },
 }
 
 # Voice and modifiers are passed in as keys to above hashes
 # Pitch and effects are passed in as params that sonic pi would use more directly
-NoteInstruction = Struct.new(:pitch, :voice, :modifiers, :effects)
+NoteInstruction = Struct.new( :pitch , :voice , :modifiers , :effects )
 
 # Responsible for rendering out the music it is given
 class Renderer
@@ -23,65 +23,71 @@ class Renderer
     @sp = sp
     @fx = {}
   end
-
+  
   # Play the instructions in the given line
   def play_instructions(instructions)
-    @sp.with_fx :level, amp: 1, amp_slide: 3 do |level|
-      @sp.with_fx :pan, pan: 0 do |pan|
-        @fx[:level] = level
-        @fx[:pan] = pan
+    @sp.with_fx :level , amp: 1, amp_slide: 3 do |level|
+      @sp.with_fx :pan , pan: 0 do |pan|
+        @fx[ :level ] = level
+        @fx[ :pan ] = pan
         instructions.each do |instruction|
-          play_note instruction
+          instruction = [instruction] if instruction.is_a?(NoteInstruction)
+          instruction.each(&method( :play_note ))
           wait instruction
         end
       end
     end
   end
-
+  
   def play_note(instruction)
     # set synth
-    if VOICES[instruction.voice][:synth]
-      @sp.use_synth VOICES[instruction.voice][:synth]
+    if VOICES[instruction.voice][ :synth ]
+      @sp.use_synth VOICES[instruction.voice][ :synth ]
     end
-
+    
     # set effects
     if instruction.effects
       instruction.effects.each do |effect|
-        @sp.control @fx[effect[:name]], effect[:opts]
+        @sp.control @fx[effect[ :name ]], effect[ :opts ]
       end
     end
-
+    
     # build note options
-    opts = VOICES[instruction.voice][:play_opts] || {}
+    opts = VOICES[instruction.voice][ :play_opts ] || {}
+    @sp.print opts
     instruction.modifiers.each do |modifier|
       if MODIFIERS[modifier]
-        opts.merge(MODIFIERS[modifier][:play_opts] || {})
+        opts = opts.merge(MODIFIERS[modifier][ :play_opts ] || {})
       end
     end
-
+    
     # play the note
     @sp.play instruction.pitch, opts
   end
-
+  
+  
   def wait(instruction)
     wait = 0.1
-    instruction.modifiers.each do |modifier|
+    instruction[0].modifiers.each do |modifier|
       if MODIFIERS[modifier].include? :wait
-        wait *= MODIFIERS[modifier][:wait]
+        wait *= MODIFIERS[modifier][ :wait ]
       end
     end
     @sp.sleep wait
   end
 end
 
+
+
 # Putting the modifiers here instead of at the top for musical reasons
 MODIFIERS = {
-  uppercase: {play_opts: {attack: 0.001, attack_level: 1, sustain: 0.05, sustain_level: 0.5, release: 0.001, amp: 3}, wait: 1.2},
-  keyword: {play_opts: {amp: 1, sustain: 0.2}, wait: 3},
-  whitespace: {wait: 2},
-  string: {play_opts: {divisor: 2.2}},
-  symbol: {play_opts: {divisor: 2.1}},
+  uppercase: { play_opts: { attack: 0.001, attack_level: 1, sustain: 0.01, sustain_level: 0.5, decay: 0.07, amp: 1.1, depth: 1.2}},
+  keyword: { play_opts: { amp: 1, sustain: 0.2}, wait: 3},
+  whitespace: { wait: 2},
+  string: { play_opts: { divisor: 2.2}},
+  symbol: { play_opts: { depth: 2.1, sustain: 0.2, attack: 0.05}},
 }
+
 
 # Responsible for parsing in and putting together instructions that the renderer takes in
 class Parser
@@ -90,48 +96,70 @@ class Parser
     @sp = sp
     @instructions = []
   end
-
+  
   def instructions
     return @instructions
   end
-
+  
   def parse_file
     # Note: This will break if the file has been renamed.
     f = File.open(File.absolute_path(__dir__) + '/reflections.rb')
-
+    
     started = false
     f.read.each_line do |line|
       if started || line.match(/#>>START-HERE\s*$/)
         started = true
         parse_line line
       end
-
+      
       break if line.match(/#>>END-HERE\s*$/)
     end
-
+    
     f.close
   end
-
+  
+  def make_indent_fx(indent)
+    return { name: :level, opts: { amp: 0.5 + (0.05 * indent)}}
+  end
+  
+  def make_groups_fx(group)
+    pan = (-0.2 * group[ :parens ])
+    pan += (0.3 * group[ :brackets ])
+    pan += (0.5 * group[ :pipes ])
+    
+    return { name: :pan, opts: { pan: pan}}
+  end
+  
+  def get_pitch(letter)
+    if letter.match(/[A-Za-z]/)
+      letters = '_abcdefghijklmnopqrstuvwxyz'
+      return letters.index(letter.downcase) + 58
+    elsif !letter.match(/([a-zA-Z]|\s)/)
+      return letter.codepoints[0]
+    end
+  end
+  
   # This thing is a monster I'm sorry
   # I like the indent depth
   def parse_line(line)
     voice = line.match(/^\s*#/) ? :comment : :normal
     is_string = false
     indent_fx = make_indent_fx line.match(/^(\s*).*$/)[1].length
-
+    
     line.each_line(' ') do |word|
       mods = []
-      groups = {parens: 0, brackets: 0, pipes: 0}
+      groups = { parens: 0, brackets: 0, pipes: 0}
       if voice != :comment && RESERVED_WORDS.include?(word)
         mods << :keyword
-      elsif word.start_with?(':') || word.end_with?(':')
+      elsif word.start_with?(':') || word.strip.end_with?(':')
         mods << :symbol
       end
-
+      word_instructions = []
+      
       word.each_char do |letter|
         char_mods = []
         char_voice = voice
-
+        
         if letter.match(/[A-Z]/)
           char_mods << :uppercase
         elsif !letter.match(/([a-zA-Z]|\s)/) && voice != :comment
@@ -141,44 +169,31 @@ class Parser
         elsif letter.match(/['"]/)
           is_string = !is_string
         end
-
+        
         if !is_string && letter == '('
-          groups[:parens] += 1;
+          groups[ :parens ] += 1;
         end
         groups_fx = make_groups_fx groups
-
+        
         char_mods << :string if is_string
-
-        @instructions << NoteInstruction.new(get_pitch(letter), char_voice, mods + char_mods, [indent_fx, groups_fx])
-
+        
+        word_instructions << NoteInstruction.new(get_pitch(letter), char_voice, mods + char_mods, [indent_fx, groups_fx])
+        
         if !is_string && letter == ')'
-          groups[:parens] -= 1;
+          groups[ :parens ] -= 1;
         end
+      end
+      
+      if mods.include?( :symbol )
+        @instructions << word_instructions
+      else
+        @instructions += word_instructions
       end
     end
   end
-
-  def make_indent_fx(indent)
-    return {name: :level, opts: {amp: 1 - (0.05 * indent)}}
-  end
-
-  def make_groups_fx(group)
-    pan = (-0.2 * group[:parens])
-    pan += (0.3 * group[:brackets])
-    pan += (0.5 * group[:pipes])
-
-    return {name: :pan, opts: {pan: pan}}
-  end
-
-  def get_pitch(letter)
-    if letter.match(/[A-Za-z]/)
-      letters = '_abcdefghijklmnopqrstuvwxyz'
-      return letters.index(letter.downcase) + 58
-    elsif !letter.match(/([a-zA-Z]|\s)/)
-      return letter.codepoints[0]
-    end
-  end
 end
+
+
 
 # Main
 
